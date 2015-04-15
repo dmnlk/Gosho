1e381e90786a268f1164ff4e1a744388aaca17af
dmnlk <seikima2demon@gmail.com>
2015-04-16 01:24:45 +0900

    fix error

--- src/main.go
+++ src/main.go
@@ -13,7 +13,7 @@ func main() {
 func getGoogleAPIKey() (string, error) {
 	api_key := os.Getenv("GOOGLE_API_KEY")
 	if stringUtils.IsEmpty(api_key) {
-		return nil, fmt.Errorf("not found api_key")
+		return "", fmt.Errorf("not found api_key")
 	}
 	return api_key, nil
 }
